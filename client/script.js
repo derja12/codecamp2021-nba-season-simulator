a0290f5bd8cdaf8334cb830d46a5792cb64d4bd2
Kaden Empey <kekag300@gmail.com>
2021-11-12 10:22:16 -0700

    huge changes

---
+++ client/script.js
@@ -0,0 +1,138 @@
+var simulateButton = document.querySelector("#simulate")
+
+simulateButton.onclick = simulateSeason;
+function simulateSeason() {
+
+}
+
+function addWesternTeams() {
+    var westernDiv = document.querySelector("western")
+    var GSWarriors = document.createElement("div")
+    var PSuns = document.createElement("div")
+    var UJazz = document.createElement("div")
+    var DMavericks = document.createElement("div")
+    var DNuggets = document.createElement("div")
+    var LAClippers = document.createElement("div")
+    var LALakers = document.createElement("div")
+    var MGrizzlies = document.createElement("div")
+    var SKings = document.createElement("div")
+    var PTrailBlazers = document.createElement("div")
+    var OCKThunder = document.createElement("div")
+    var SASpurs = document.createElement("div")
+    var MTimberwolves = document.createElement("div")
+    var HRockets = document.createElement("div")
+    var NOPelicans = document.createElement("div")
+    GSWarriors.id = "warriors"
+    GSWarriors.classList.add('team-div')
+    PSuns.id = "suns"
+    PSuns.classList.add('team-div')
+    UJazz.id = "jazz"
+    UJazz.classList.add('team-div')
+    DMavericks.id = "mavericks"
+    DMavericks.classList.add('team-div')
+    DNuggets.id = "nuggets"
+    DNuggets.classList.add('team-div')
+    LAClippers.id = "clippers"
+    LAClippers.classList.add('team-div')
+    LALakers.id = "lakers"
+    LALakers.classList.add('team-div')
+    MGrizzlies.id = "grizzlies"
+    MGrizzlies.classList.add('team-div')
+    SKings.id = "kings"
+    SKings.classList.add('team-div')
+    PTrailBlazers.id = "trailblazers"
+    PTrailBlazers.classList.add('team-div')
+    OCKThunder.id = "thunder"
+    OCKThunder.classList.add('team-div')
+    SASpurs.id = "spurs"
+    SASpurs.classList.add('team-div')
+    MTimberwolves.id = "timberwolves"
+    MTimberwolves.classList.add('team-div')
+    HRockets.id = "rockets"
+    HRockets.classList.add('team-div')
+    NOPelicans.id = "pelicans"
+    NOPelicans.classList.add('team-div')
+    westernDiv.appendChild(GSWarriors)
+    westernDiv.appendChild(PSuns)
+    westernDiv.appendChild(UJazz)
+    westernDiv.appendChild(DNuggets)
+    westernDiv.appendChild(LAClippers)
+    westernDiv.appendChild(LALakers)
+    westernDiv.appendChild(MGrizzlies)
+    westernDiv.appendChild(SKings)
+    westernDiv.appendChild(PTrailBlazers)
+    westernDiv.appendChild(OCKThunder)
+    westernDiv.appendChild(SASpurs)
+    westernDiv.appendChild(MTimberwolves)
+    westernDiv.appendChild(HRockets)
+    westernDiv.appendChild(DMavericks)
+    westernDiv.appendChild(NOPelicans)
+}
+
+function addEasternTeams() {
+    var WWizards = document.createElement("div")
+    var CBulls = document.createElement("div")
+    var NJNets = document.createElement("div")
+    var P76ers = document.createElement("div")
+    var CCavaliers = document.createElement("div")
+    var MHeat = document.createElement("div")
+    var TRaptors = document.createElement("div")
+    var MBucks = document.createElement("div")
+    var CHornets = document.createElement("div")
+    var BCeltics = document.createElement("div")
+    var IPacers = document.createElement("div")
+    var AHawks = document.createElement("div")
+    var OMagic = document.createElement("div")
+    var DPistons = document.createElement("div")
+    var NYKnicks = document.createElement("div")
+    WWizards.id = "wizards"
+    WWizards.classList.add('team-div')
+    CBulls.id = "bulls"
+    CBulls.classList.add('team-div')
+    NJNets.id = "nets"
+    NJNets.classList.add('team-div')
+    P76ers.id = "76ers"
+    P76ers.classList.add('team-div')
+    CCavaliers.id = "cavaliers"
+    CCavaliers.classList.add('team-div')
+    MHeat.id = "heat"
+    MHeat.classList.add('team-div')
+    TRaptors.id = "raptors"
+    TRaptors.classList.add('team-div')
+    MBucks.id = "bucks"
+    MBucks.classList.add('team-div')
+    CHornets.id = "hornets"
+    CHornets.classList.add('team-div')
+    BCeltics.id = "celtics"
+    BCeltics.classList.add('team-div')
+    IPacers.id = "pacers"
+    IPacers.classList.add('team-div')
+    AHawks.id = "hawks"
+    AHawks.classList.add('team-div')
+    OMagic.id = "magics"
+    OMagic.classList.add('team-div')
+    DPistons.id = "pistons"
+    DPistons.classList.add('team-div')
+    NYKnicks.id = "knicks"
+    NYKnicks.classList.add('team-div')
+}
+
+var simulateButton = document.querySelector("#simulate")
+
+simulateButton.onclick = simulateSeason;
+function simulateSeason() {
+
+}
+
+/*
+
+functions:
+
+onDropDownClicked():
+    - extends a div beneath the team label
+    - fills the div with the team information
+    - closes an already opened div
+
+onSimulateButtonClicked():
+    - 
+*/
