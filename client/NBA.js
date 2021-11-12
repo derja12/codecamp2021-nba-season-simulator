6dd72df994ec7e5fc7719cec9e52467c395aebcc
Brett Hemingway <bretthemingway@Bretts-MacBook-Pro.local>
2021-11-12 10:04:04 -0700

    brett

--- client/NBA.js
+++ client/NBA.js
@@ -14,7 +14,7 @@ function addWesternTeams()
 {
     var GSWarriors = document.createElement("div")
     var PSuns = document.createElement("div")
-    var UJAZZ = document.createElement("div")
+    var UJazz = document.createElement("div")
     var DMavericks = document.createElement("div")
     var DNuggets = document.createElement("div")
     var LAClippers = document.createElement("div")
@@ -27,7 +27,87 @@ function addWesternTeams()
     var MTimberwolves = document.createElement("div")
     var HRockets = document.createElement("div")
     var NOPelicans = document.createElement("div")
+    GSWarriors.id = "warriors"
+    GSWarriors.classList.add('team-div')
+    PSuns.id = "suns"
+    PSuns.classList.add('team-div')
+    UJazz.id = "jazz"
+    UJazz.classList.add('team-div')
+    DMavericks.id = "mavericks"
+    DMavericks.classList.add('team-div')
+    DNuggets.id = "nuggets"
+    DNuggets.classList.add('team-div')
+    LAClippers.id = "clippers"
+    LAClippers.classList.add('team-div')
+    LALakers.id = "lakers"
+    LALakers.classList.add('team-div')
+    MGrizzlies.id = "grizzlies"
+    MGrizzlies.classList.add('team-div')
+    SKings.id = "kings"
+    SKings.classList.add('team-div')
+    PTrailBlazers.id = "trailblazers"
+    PTrailBlazers.classList.add('team-div')
+    OCKThunder.id = "thunder"
+    OCKThunder.classList.add('team-div')
+    SASpurs.id = "spurs"
+    SASpurs.classList.add('team-div')
+    MTimberwolves.id = "timberwolves"
+    MTimberwolves.classList.add('team-div')
+    HRockets.id = "rockets"
+    HRockets.classList.add('team-div')
+    NOPelicans.id = "pelicans"
+    NOPelicans.classList.add('team-div')
 
+
+}
+
+function addEasternTeams()
+{
+    var WWizards = document.createElement("div")
+    var CBulls = document.createElement("div")
+    var NJNets = document.createElement("div")
+    var P76ers = document.createElement("div")
+    var CCavaliers = document.createElement("div")
+    var MHeat = document.createElement("div")
+    var TRaptors = document.createElement("div")
+    var MBucks = document.createElement("div")
+    var CHornets = document.createElement("div")
+    var BCeltics = document.createElement("div")
+    var IPacers = document.createElement("div")
+    var AHawks = document.createElement("div")
+    var OMagic = document.createElement("div")
+    var DPistons = document.createElement("div")
+    var NYKnicks = document.createElement("div")
+    WWizards.id = "wizards"
+    WWizards.classList.add('team-div')
+    CBulls.id = "bulls"
+    CBulls.classList.add('team-div')
+    NJNets.id = "nets"
+    NJNets.classList.add('team-div')
+    P76ers.id = "76ers"
+    P76ers.classList.add('team-div')
+    CCavaliers.id = "cavaliers"
+    CCavaliers.classList.add('team-div')
+    MHeat.id = "heat"
+    MHeat.classList.add('team-div')
+    TRaptors.id = "raptors"
+    TRaptors.classList.add('team-div')
+    MBucks.id = "bucks"
+    MBucks.classList.add('team-div')
+    CHornets.id = "hornets"
+    CHornets.classList.add('team-div')
+    BCeltics.id = "celtics"
+    BCeltics.classList.add('team-div')
+    IPacers.id = "pacers"
+    IPacers.classList.add('team-div')
+    AHawks.id = "hawks"
+    AHawks.classList.add('team-div')
+    OMagic.id = "magics"
+    OMagic.classList.add('team-div')
+    DPistons.id = "pistons"
+    DPistons.classList.add('team-div')
+    NYKnicks.id = "knicks"
+    NYKnicks.classList.add('team-div')
 
 
 }
