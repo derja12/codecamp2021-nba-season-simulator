20bae8eb648a675c33ea750bbd197ab9a23f375e
Brett Hemingway <bretthemingway@Bretts-MacBook-Pro.local>
2021-11-12 09:46:48 -0700

    yup

--- client/NBA.js
+++ client/NBA.js
@@ -8,6 +8,28 @@ function simulateSeason()
 {
 
 }
+
+function addWesternTeams()
+{
+    var GSWarriors = document.createElement("div")
+    var PSuns = document.createElement("div")
+    var UJAZZ = document.createElement("div")
+    var DMavericks = document.createElement("div")
+    var DNuggets = document.createElement("div")
+    var LAClippers = document.createElement("div")
+    var LALakers = document.createElement("div")
+    var MGrizzlies = document.createElement("div")
+    var SKings = document.createElement("div")
+    var PTrailBlazers = document.createElement("div")
+    var OCKThunder = document.createElement("div")
+    var SASpurs = document.createElement("div")
+    var MTimberwolves = document.createElement("div")
+    var HRockets = document.createElement("div")
+    var NOPelicans = document.createElement("div")
+
+
+
+}
 =======
 
 /*
